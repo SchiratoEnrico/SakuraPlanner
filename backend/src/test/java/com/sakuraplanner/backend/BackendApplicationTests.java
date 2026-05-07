fa1091d52b73ff2f11a93a138df7a56ed06cfa69
Boiaca <boiaca2003@gmail.com>
2026-05-07 16:30:03 +0200

    Initial commit: setup backend and frontend folders

---
+++ backend/src/test/java/com/sakuraplanner/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sakuraplanner.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
