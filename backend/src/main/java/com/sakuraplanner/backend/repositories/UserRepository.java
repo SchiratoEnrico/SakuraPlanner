b2633595a1862122b245aef1882d2b8142fbc239
SchiratoEnrico <97945290+SchiratoEnrico@users.noreply.github.com>
2026-06-19 15:34:47 +0200

    Create UserRepository.java
    repos

---
+++ backend/src/main/java/com/sakuraplanner/backend/repositories/UserRepository.java
@@ -0,0 +1,19 @@
+package com.sakuraplanner.repository;
+
+import com.sakuraplanner.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByUsername(String username);
+
+  //useful during user verification
+    boolean existsByEmail(String email);
+    
+    boolean existsByUsername(String username);
+}
